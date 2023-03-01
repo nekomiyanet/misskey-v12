12caab9f36d5157028ce5428cae4107366a06b8b
atsu1125 <atsuchan@atsuchan.page>
2023-03-01 16:37:51 +0900

    Fix: notes/renotes

--- packages/backend/src/server/api/endpoints/notes/renotes.ts
+++ packages/backend/src/server/api/endpoints/notes/renotes.ts
@@ -66,7 +66,7 @@ export default define(meta, paramDef, async (ps, user) => {
 	generateVisibilityQuery(query, user);
 	if (user) generateMutedUserQuery(query, user);
 	if (user && !user.isAdmin && !user.isModerator) {
-		generateBlockedUserQuery(query, me);
+		generateBlockedUserQuery(query, user);
 	}
 
 	const renotes = await query.take(ps.limit).getMany();
