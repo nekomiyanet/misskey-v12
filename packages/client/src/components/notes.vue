00ab5d445a8e6c39e44e3cf9b3bd470d486aa7aa
Kon Kon Tannu Morote <kon2tannu@yandex.ru>
2023-08-14 17:12:03 +0900

    Change error&info image (#3)
    (cherry picked from commit b3916908b0813f94e478c8d6b1e46bcc8976a452)

--- packages/client/src/components/notes.vue
+++ packages/client/src/components/notes.vue
@@ -2,7 +2,7 @@
 <MkPagination ref="pagingComponent" :pagination="pagination">
 	<template #empty>
 		<div class="_fullinfo">
-			<img src="https://xn--931a.moe/assets/info.jpg" class="_ghost"/>
+			<img src="https://portal.nekomiya.net/assets/info.gif" class="_ghost"/>
 			<div>{{ $ts.noNotes }}</div>
 		</div>
 	</template>
--- packages/client/src/pages/settings/apps.vue
+++ packages/client/src/pages/settings/apps.vue
@@ -3,7 +3,7 @@
 	<FormPagination ref="list" :pagination="pagination">
 		<template #empty>
 			<div class="_fullinfo">
-				<img src="https://xn--931a.moe/assets/info.jpg" class="_ghost"/>
+				<img src="https://portal.nekomiya.net/assets/info.gif" class="_ghost"/>
 				<div>{{ $ts.nothing }}</div>
 			</div>
 		</template>
