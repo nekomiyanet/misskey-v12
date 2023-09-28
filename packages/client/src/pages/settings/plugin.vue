cfe8c2b63241cf4da988afdcb8b7832dd551a085
atsu1125 <atsuchan@atsuchan.page>
2023-09-28 19:29:17 +0900

    Fix: Showing Permission of plugin

--- packages/client/src/pages/settings/plugin.vue
+++ packages/client/src/pages/settings/plugin.vue
@@ -19,7 +19,12 @@
 			</MkKeyValue>
 			<MkKeyValue class="_formBlock">
 				<template #key>{{ $ts.permission }}</template>
-				<template #value>{{ plugin.permission }}</template>
+				<template #value>
+					<ul style="margin-top: 0; margin-bottom: 0;">
+						<li v-for="permission in plugin.permissions" :key="permission">{{ $ts._permissions[permission] }}</li>
+						<li v-if="!plugin.permissions || plugin.permissions.length === 0">{{ $ts.none }}</li>
+					</ul>
+				</template>
 			</MkKeyValue>
 
 			<div style="display: flex; gap: var(--margin); flex-wrap: wrap;">
