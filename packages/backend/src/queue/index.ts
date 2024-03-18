16de54cafafa92647165b6f544ad55800e56f15d
atsu1125 <atsuchan@atsuchan.page>
2024-03-18 21:21:14 +0900

    Tune: account deletion

--- packages/backend/src/queue/index.ts
+++ packages/backend/src/queue/index.ts
@@ -269,9 +269,9 @@ export function createDeleteAccountJob(user: ThinUser, opts: { soft?: boolean; }
 		user: user,
 		soft: opts.soft,
 	}, {
-		attempts: 3,
+		attempts: 6,
 		removeOnComplete: true,
-		removeOnFail: true,
+		removeOnFail: false,
 	});
 }
 
