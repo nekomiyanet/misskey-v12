26defe5b8231b785fac2d792a5e2582b801160ce
mei23 <m@m544.net>
2024-03-26 21:05:29 +0900

    fix (backend): improve URL check
    https://github.com/mei23/misskey/commit/13ea67bee40b394906f55e75d5d80d6e4f7fa171
    https://github.com/mei23/misskey/commit/da12d5b079b690d3705a76919ed817f6dfc03ac8
    
    Co-authored-by: naskya <m@naskya.net>

--- packages/backend/src/misc/fetch.ts
+++ packages/backend/src/misc/fetch.ts
@@ -5,6 +5,7 @@ import fetch from 'node-fetch';
 import { HttpProxyAgent, HttpsProxyAgent } from 'hpagent';
 import config from '@/config/index.js';
 import { URL } from 'node:url';
+import { isValidUrl } from './is-valid-url.js';
 
 export async function getJson(url: string, accept = 'application/json, */*', timeout = 10000, headers?: Record<string, string>) {
 	const res = await getResponse({
@@ -34,8 +35,19 @@ export async function getHtml(url: string, accept = 'text/html, */*', timeout = 
 	return await res.text();
 }
 
-export async function getResponse(args: { url: string, method: string, body?: string, headers: Record<string, string>, timeout?: number, size?: number }) {
-	const timeout = args?.timeout || 10 * 1000;
+export async function getResponse(args: {
+	url: string;
+	method: string;
+	body?: string;
+	headers: Record<string, string>;
+	timeout?: number;
+	size?: number;
+}) {
+	if (!isValidUrl(args.url)) {
+		throw new StatusError('Invalid URL', 400);
+	}
+
+	const timeout = args.timeout || 10 * 1000;
 
 	const controller = new AbortController();
 	setTimeout(() => {
@@ -54,6 +66,10 @@ export async function getResponse(args: { url: string, method: string, body?: st
 
 	if (!res.ok) {
 		throw new StatusError(`${res.status} ${res.statusText}`, res.status, res.statusText);
+	}
+
+	if (res.redirected && !isValidUrl(res.url)) {
+		throw new StatusError('Invalid URL', 400);
 	}
 
 	return res;
