cc9e257791d8d07c88da9f1121945becdfd29cd0
atsu1125 <atsuchan@atsuchan.page>
2024-06-03 16:49:04 +0900

    enhance (backend): setting of remote users chart

--- packages/backend/src/services/note/reaction/create.ts
+++ packages/backend/src/services/note/reaction/create.ts
@@ -14,6 +14,7 @@ import deleteReaction from './delete.js';
 import { isDuplicateKeyValueError } from '@/misc/is-duplicate-key-value-error.js';
 import { NoteReaction } from '@/models/entities/note-reaction.js';
 import { IdentifiableError } from '@/misc/identifiable-error.js';
+import config from '@/config/index.js';
 
 export default async (user: { id: User['id']; host: User['host']; }, note: Note, reaction?: string) => {
 	// Check blocking
@@ -71,7 +72,9 @@ export default async (user: { id: User['id']; host: User['host']; }, note: Note,
 		.where('id = :id', { id: note.id })
 		.execute();
 
-	perUserReactionsChart.update(user, note);
+	if (!config.disableChartsForRemoteUser || (user.host == null)) {
+		perUserReactionsChart.update(user, note);
+	}
 
 	// カスタム絵文字リアクションだったら絵文字情報も送る
 	const decodedReaction = decodeReaction(reaction);
