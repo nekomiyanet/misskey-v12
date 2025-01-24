b44a9d7a0aa1498d8d486a61041cc7022fb7f5b5
atsu1125 <atsuchan@atsuchan.page>
2025-01-24 20:48:05 +0900

    fix(backend): inbox skip activity id is not a string

--- packages/backend/src/queue/processors/inbox.ts
+++ packages/backend/src/queue/processors/inbox.ts
@@ -142,6 +142,8 @@ export default async (job: Bull.Job<InboxJobData>): Promise<string> => {
 		if (signerHost !== activityIdHost) {
 			return `skip: signerHost(${signerHost}) !== activity.id host(${activityIdHost}`;
 		}
+	} else {
+		return `skip: activity id is not a string`;
 	}
 
 	// Update stats
