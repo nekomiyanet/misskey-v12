24defd37a3a30f21ae15b7da32e823d933b93a9c
atsu1125 <atsuchan@atsuchan.page>
2025-05-06 10:19:41 +0900

    fix(client): Reject access parent path for Mk:api

--- packages/client/src/scripts/aiscript/api.ts
+++ packages/client/src/scripts/aiscript/api.ts
@@ -25,7 +25,7 @@ export function createAiScriptEnv(opts) {
 		}),
 		'Mk:api': values.FN_NATIVE(async ([ep, param, token]) => {
 			utils.assertString(ep);
-			if (ep.value.includes('://')) throw new Error('invalid endpoint');
+			if (ep.value.includes('://') || ep.value.includes('..')) throw new Error('invalid endpoint');
 			if (token) {
 				utils.assertString(token);
 				// バグがあればundefinedもあり得るため念のため
