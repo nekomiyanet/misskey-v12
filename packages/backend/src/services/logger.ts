bf63523ce7105c9163f503ee28ae45f6b6725f86
atsu1125 <atsuchan@atsuchan.page>
2026-07-15 13:25:03 +0900

    fix(backend): syslog typo

--- packages/backend/src/services/logger.ts
+++ packages/backend/src/services/logger.ts
@@ -29,7 +29,7 @@ export default class Logger {
 
 		if (config.syslog) {
 			this.syslogClient = new SyslogPro.RFC5424({
-				applacationName: 'Misskey',
+				applicationName: 'Misskey',
 				timestamp: true,
 				encludeStructuredData: true,
 				color: true,
